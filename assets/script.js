b0ae26da2d25b762451d4ee0b54afbca27eecc1f
pratyusha2802 <pratyushadipesh@gmail.com>
2024-02-15 23:13:33 +0530

    st;yling changes

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,23 @@
-const navbarToggle = document.getElementById('navbarToggle');
-const navbar = document.getElementById('navbar');
+const navbarToggle = document.getElementById("navbarToggle")
+const navbar = document.getElementById("navbar")
 
-navbarToggle.addEventListener('click', () => {
-  navbar.classList.toggle('active');
-  navbarToggle.classList.toggle('active');
-});
+navbarToggle.addEventListener("click", () => {
+  navbar.classList.toggle("active")
+  navbarToggle.classList.toggle("active")
+})
+
+const videos = document.querySelectorAll(".video")
+let currentVideo = 0
+
+function playNextVideo() {
+  videos[currentVideo].muted = true
+  videos[currentVideo].pause()
+
+  currentVideo = (currentVideo + 1) % videos.length
+  videos[currentVideo].muted = false
+  videos[currentVideo].play()
+
+  videos[currentVideo].addEventListener("ended", playNextVideo)
+}
+
+playNextVideo()
