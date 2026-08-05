f8a5a756f5fe3545646b567af9388a433642a60a
pratyusha2802 <pratyushadipesh@gmail.com>
2026-08-05 06:09:38 +0530

    Revert "Break up card-grid monotony with real, non-fabricated visual variety"
    This reverts commit 04f5e84f9e2eac9fe814495719c31af703719f17.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,6 @@ import Currently from "../components/Currently";
 import Experience from "../components/Experience";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
-import StickyQuote from "../components/StickyQuote";
 import { profile } from "../data/portfolio";
 import { useRiseAnimation } from "../lib/useRiseAnimation";
 
@@ -22,9 +21,6 @@ function Home() {
       <Hero />
       <Currently />
       <CaseStudies />
-      <StickyQuote source="Bangalore Food Bank case study">
-        This hackathon is also how I ended up at JPMorgan Chase, which I didn't plan going in.
-      </StickyQuote>
       <About />
       <Experience />
       <Background />
